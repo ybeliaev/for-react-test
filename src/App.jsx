49cfa2e1985a95310d46a639417a9d693bb382ef
Jorgen <ybeliaev@gmail.com>
2020-10-27 21:31:00 +0200

    tutor useState 1

--- src/App.jsx
+++ src/App.jsx
@@ -6,17 +6,29 @@ import "./App.scss";
 
 
 const HookSweacher = () => {
+  const [color, setColor] = React.useState("black");
+  const [fontSize, setFontSize] = React.useState(20);
+  const [person, setPerson] = React.useState({
+    firstName: "joy",
+    lastName: "Niga",
+  })
   const styles = {
     div:{
       padding: 50,
-      backgroundColor: "white"
+      backgroundColor: color,
+      fontSize: `${fontSize}px`,
     }
   }
+  setPerson((person) => {
+    return {...person,firstName: "Joy"}
+  })
 
   return(
     <div style={styles.div}>
-      <button onClick = {()=>{}}>Dark</button>
-      <button onClick = {()=>{}}>Light</button>
+      <p>Lol</p>
+      <button onClick = {()=>{setColor("black")}}>Dark</button>
+      <button onClick = {()=>{setColor("yellow")}}>Light</button>
+      <button onClick = {()=>{setFontSize(s => s + 2)}}>+</button>
     </div>
   )
 }
