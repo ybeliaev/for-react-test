73e26627c1022ac30eac1975ab536140326ef4e6
Jorgen <ybeliaev@gmail.com>
2020-10-27 20:33:52 +0200

    tutor useState

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
-Only for testing code parts
+Only for testing code parts.
+
+Use `yarn` in this project.
--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,33 @@
 import React from "react";
+
+import clsx from "clsx";
+
 import "./App.scss";
 
+
+const HookSweacher = () => {
+  const styles = {
+    div:{
+      padding: 50,
+      backgroundColor: "white"
+    }
+  }
+
+  return(
+    <div style={styles.div}>
+      <button onClick = {()=>{}}>Dark</button>
+      <button onClick = {()=>{}}>Light</button>
+    </div>
+  )
+}
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <h1>Lol</h1>
+        <HookSweacher/>
       </header>
     </div>
   );
