4ab354be55e6ddd8ce3ed813c1a32a0d0c52c16a
Jorgen <ybeliaev@gmail.com>
2020-10-27 12:07:13 +0200

    some emprove

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import React from "react";
 import "./App.scss";
-import { ClickCounter } from "./components/ClickCounter";
-import { List } from "./components/List";
 
-const arr = [1, 2, 3, 4, 5];
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <List items={arr} />
-        <ClickCounter />
+        
       </header>
     </div>
   );
