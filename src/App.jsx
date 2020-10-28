c675a85a7d4cd27feed0be903de4a9b4847a2c3f
Jorgen <ybeliaev@gmail.com>
2020-10-28 20:58:44 +0200

    useEffect-5

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,11 @@ import {HookSweacher} from "./components/1-useState"
 import {Child} from "./components/2-useContext"
 import {EffectComponent} from "./components/3-useEffect"
 import {Notification} from "./components/4-useEffect"
+import {PlanetInfo} from "./components/5-useEffect"
 
 
 function App() {
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +19,7 @@ function App() {
           <Child />  
           <EffectComponent /> 
           <Notification />     
+          <PlanetInfo />
       </header>
     </div>
   );
---
+++ src/components/5-useEffect.jsx
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react"
+
+export const PlanetInfo = () => {
+    const [name, setName] = useState(null)
+    const [id, setID] = useState(1)
+
+    useEffect(()=>{
+        let cancelled = false
+        fetch(`http://swapi.dev/api/planets/${id}/`)
+        .then(res => res.json())
+        .then(data => !cancelled && setName(data.name))
+        return () => cancelled = true
+    }, [id])
+    
+    return(
+    <div style={{backgroundColor:"Brown", padding: 10, width: '100%'}}>
+        <h2>5-useEffect для загрузки данных.</h2>
+        <p>{id} - {name} Planet </p>
+        <button onClick = {()=>{setID(i => i + 1)}}>ADD</button>
+        <button onClick = {()=>{setID(i => i = 1)}}>RESET</button>
+    </div>
+    )
+}
