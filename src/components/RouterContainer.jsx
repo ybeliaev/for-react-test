b9742cec795f0a0979dd92582ee6385ae67f68a0
Jorgen <ybeliaev@gmail.com>
2020-10-30 17:26:45 +0200

    route exact

--- src/components/RouterContainer.jsx
+++ src/components/RouterContainer.jsx
@@ -9,18 +9,23 @@ export const RouterContainer = () => {
     return(
         <Router>
             <div style={{minHeight: 400, width: "100%", backgroundColor: "Ivory", padding: 10}}>
-            <nav >
-                <ul style={{listStyle:"none"}}>
-                    <li><Link to="/coupon">Coupon</Link></li>
-                    <li><Link to="/table">Table</Link></li>
-                    <li><Link to="/">Home</Link></li>
-                </ul>
-            </nav>
-            <div>                
-                <Route path="/coupon" component={RouterCoupon} />
-                <Route path="/table" component={RouterTable} />
-                
-            </div>
+                <nav >
+                    <ul style={{listStyle:"none"}}>
+                        <li><Link to="/">Home</Link></li>
+                        <li><Link to="/coupon">Coupon</Link></li>
+                        <li><Link to="/table">Table</Link></li>
+                    </ul>
+                </nav>
+                <div>
+                    <Route path="/" 
+                        exact
+                        render={()=><h2 style={{color: "black"}}>8-Знакомство с роутером.</h2>} /> 
+                    {/* роутер смотрит или СОДЕРЖИТСЯ "/" а не точно совпадает, поэтому всегда будет надпись */}
+                    <Route path="/coupon" render={()=><h3 style={{color: "black"}}>Роуты накладываются</h3>} />
+                    <Route path="/coupon" component={RouterCoupon} />
+                    <Route path="/table" component={RouterTable} />
+                    
+                </div>
             </div>
             
         </Router>
--- src/components/RouterCoupon.jsx
+++ src/components/RouterCoupon.jsx
@@ -11,7 +11,7 @@ export const RouterCoupon = () => {
         <img src={logo} alt="Avatar" style={{width:"100%"}} />
         <div className="container" style={{backgroundColor:'white'}}>
             <h2><b>20% OFF YOUR PURCHASE</b></h2> 
-            <p>Lorem ipsum dolor sit amet..</p>
+            <p>Testy burger yam-yam..</p>
         </div>
         <div className="container">
             <p>Use Promo Code: <span className={"promo"}>BOH232</span></p>
